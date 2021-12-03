e05302984fc2da25518cc893ed1a9c82c18a1f1e
aaron <afarrell002@gmail.com>
2021-12-02 19:45:08 -0700

    updated functionality - accepts user input and creates an html doc

--- index.js
+++ index.js
@@ -1,71 +1,117 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const writeToFile = require("./src/src");
+
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+
+const src = require("./src/src");
 
 const questions = [
   {
     type: "input",
-    message: "What is your name?",
+    message: "What is your employee's name?",
     name: "name",
   },
   {
     type: "input",
-    message: "What is your employee ID?",
+    message: "What is your employee's ID?",
     name: "empID",
   },
   {
     type: "input",
-    message: "What is your email address?",
+    message: "What is your employee's email address?",
     name: "email",
   },
   {
     type: "list",
-    message: "Please choose your role:",
+    message: "Please choose your employee's role:",
     choices: ["Manager", "Engineer", "Intern"],
     name: "role",
   },
 ];
 
 function init() {
-  inquirer.prompt(questions).then((userResponse) =>
+  inquirer.prompt(questions).then((answers) =>
     inquirer
       .prompt([
         {
           type: "input",
-          message: "What is your office number?",
-          name: "officeNumber",
+          message: "What is your manager's office number?",
+          name: "office",
           when: function (_) {
-            return userResponse.role === "Manager";
+            return answers.role === "Manager";
           },
         },
         {
           type: "input",
-          message: "What is your GutHub username?",
-          name: "githubUsername",
+          message: "What is your engineer's GutHub username?",
+          name: "github",
           when: function (_) {
-            return userResponse.role === "Engineer";
+            return answers.role === "Engineer";
           },
         },
         {
           type: "input",
-          message: "What school do you attend??",
+          message: "What school does your intern attend?",
           name: "school",
           when: function (_) {
-            return userResponse.role === "Intern";
+            return answers.role === "Intern";
           },
         },
       ])
-
-      .then(({ officeNumber, githubUsername, school }) => {
-        fs.writeFile(
-          "./dist/index.html",
-          writeToFile(userResponse, officeNumber, githubUsername, school),
-          (err) => {
-            err ? console.log(err) : console.log("File was written!");
-          }
-        );
+      .then(({ office, github, school }) => {
+        createEmployee(answers, office, github, school);
       })
   );
 }
+
+const createEmployee = (answers, office, github, school) => {
+  if (answers.role === "Manager") {
+    src.team;
+    const newManager = new Manager(
+      answers.name,
+      answers.empID,
+      answers.email,
+      office
+    );
+    src.team.push(newManager);
+    addEmp();
+  } else if (answers.role === "Engineer") {
+    const newEngineer = new Engineer(
+      answers.name,
+      answers.empID,
+      answers.email,
+      github
+    );
+    src.team.push(newEngineer);
+    addEmp();
+  } else if (answers.role === "Intern") {
+    const newIntern = new Intern(
+      answers.name,
+      answers.empID,
+      answers.email,
+      school
+    );
+    src.team.push(newIntern);
+    addEmp();
+  }
+};
+
+const addEmp = async () => {
+  const answer = await inquirer.prompt([
+    {
+      type: "confirm",
+      message: "Would you like to add another employee?",
+      name: "addEmp",
+    },
+  ]);
+  if (answer.addEmp === true) {
+    src.grabTeamString;
+    init();
+  } else {
+    src.createTeam();
+  }
+};
 
 init();
--- src/src.js
+++ src/src.js
@@ -1,28 +1,104 @@
+const team = [];
 const fs = require("fs");
 
-function writeToFile(userResponse, officeNumber, githubUsername, school) {
-  return `
-  ${userResponse.name}
-  ${userResponse.email}
-  ${userResponse.empID}
-  ${userResponse.role}
-  ${officeNumber}
-  ${githubUsername}
-  ${school}`;
-}
+const managerCard = (emp) => {
+  const manager = `
+    <div class="card" style="width: 18rem">
+      <div class="card-header"> Manager </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item"> Name: ${emp.getName()}</li>
+          <li class="list-group-item"> ID Number: ${emp.getId()}</li>
+          <a href="mailto:${emp.getEmail()}" ><li class="list-group-item"> Email: ${emp.getEmail()}</li></a>
+          <li class="list-group-item"> Office Number: ${emp.getOfficeNumber()}</li>
+        </ul>
+  </div>`;
+  return manager;
+};
 
-module.exports = writeToFile;
+const engineerCard = (emp) => {
+  const engineer = `
+    <div class="card" style="width: 18rem">
+      <div class="card-header"> Engineer </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item"> Name: ${emp.getName()}</li>
+          <li class="list-group-item"> ID Number: ${emp.getId()}</li>
+          <a href="mailto:${emp.getEmail()}" ><li class="list-group-item"> Email: ${emp.getEmail()}</li></a>
+          <li class="list-group-item"> Office Number: ${emp.getGithub()}</li>
+        </ul>
+  </div>`;
+  return engineer;
+};
+const internCard = (emp) => {
+  const intern = `
+    <div class="card" style="width: 18rem">
+      <div class="card-header"> Intern </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item"> Name: ${emp.getName()}</li>
+          <li class="list-group-item"> ID Number: ${emp.getId()}</li>
+          <a href="mailto:${emp.getEmail()}" ><li class="list-group-item"> Email: ${emp.getEmail()}</li></a>
+          <li class="list-group-item"> Office Number: ${emp.getSchool()}</li>
+        </ul>
+  </div>`;
+  return intern;
+};
 
-/* 
-template literal to build the base of thml document, then add another template literal via a variable house in an expression.
+const grabTeamString = (team) => {
+  let cardTemp = "";
+  team.forEach((emp) => {
+    switch (emp.getRole()) {
+      case "Manager":
+        cardTemp += managerCard(emp);
+        break;
+      case "Engineer":
+        cardTemp += engineerCard(emp);
+        break;
+      case "Intern":
+        cardTemp += internCard(emp);
+        break;
+    }
+  });
 
-ie - `html doc
-html doc
-html doc
-${card1}
-html doc
-html doc`
+  const indexHTML = `<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          rel="stylesheet"
+          href=""https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css""
+        />
+        <link rel="stylesheet" href="./dist/style.css" />
+        <title>Generate My Team!</title>
+      </head>
+      <body>
+        
+        <header class="jumbotron jumbotron-fluid">
+          <div class="container">
+            <h1 class="display-4">My Team</h1>
+          </div>
+        </header>
+        <section class="sect-container">
+          ${cardTemp}
+        </section>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"></script>
+      </body>
+    </html>
+  `;
 
+  return indexHTML;
+};
 
-const card1 = `template literal that builds the card`
-*/
+const createTeam = () => {
+  fs.writeFile("./dist/index.html", grabTeamString(team), (err) => {
+    if (err) {
+      err ? console.log(err) : console.log("File was written!");
+    }
+  });
+};
+
+module.exports = {
+  team,
+  grabTeamString,
+  createTeam,
+};
